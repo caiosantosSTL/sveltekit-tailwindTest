1ab67c8e600664868ae333e50a7d51ab2494c982
Caio Santos da Silva <vorreicaio@hotmail.com>
2022-02-07 15:32:37 -0600

    sveltekit with tailwind and daisyui

---
+++ src/navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<main>
+    <ul>
+        <li>
+            sdasda
+        </li>
+        <li>
+            sdasda
+        </li>
+        <li>
+            sdasda
+        </li>
+    </ul>
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,87 @@
+
+<script>
+    import Navbar from '../navbar.svelte'
+</script>
+
+<main>
+    <section>
+        <div class="container mx-auto flex justify-center">
+            <button class="text-3xl font-light bg-red-500 hover:bg-blue-200
+                  active:transform active:translate-y-2  
+                  p-3 my-4 rounded-2xl transition-all">Hello world!
+            </button>
+        </div>
+        <Navbar/>
+    </section>
+
+    <section class="bg-black ">
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-1">
+            <div class="bg-white m-4 p-4 rounded flex flex-col flex-wrap justify-center">
+                <div>
+                    <h3>sdasd</h3>
+                </div>
+                <div class="bg-red-300 mt-2 rounded flex justify-center">
+                    <div class="collapse border rounded-box border-base-300 collapse-arrow">
+                        <input type="checkbox"> 
+                        <div class="collapse-title text-xl font-medium">
+                          I open/close with click
+                        </div> 
+                        <div class="collapse-content"> 
+                          <p>Collapse content reveals with focus. If you add a checkbox, you can control it using checkbox instead of focus. Or you can force-open/force-close using 
+                            <span class="badge badge-outline">collapse-open</span> and 
+                            <span class="badge badge-outline">collapse-close</span> classes.
+                          </p>
+                        </div>
+                      </div>
+                </div>
+            </div>
+            <div class="bg-white m-4 p-4 rounded flex flex-col justify-center">
+                <div>
+                    <h3>sdasd</h3>
+                </div>
+                <div class="bg-red-700 mt-2 rounded">
+                    sdljf
+                </div>
+            </div>
+            <div class="bg-white m-4 p-4 rounded flex flex-col justify-center">
+                <div>
+                    <h3>sdasd</h3>
+                </div>
+                <div class="bg-red-700 mt-2 rounded flex justify-center">
+                    <button
+                    class="inline-block px-4 py-3 text-sm font-semibold text-center text-white uppercase transition duration-200 ease-in-out bg-indigo-500 rounded-md cursor-pointer hover:bg-indigo-600">Tailwind
+                    Button</button>
+                </div>
+            </div>
+            <div class="bg-white m-4 p-4 rounded flex flex-col justify-center">
+                <div>
+                    <h3>sdasd</h3>
+                </div>
+                <div class="bg-red-700 mt-2 rounded">
+                    <div tabindex="0" class="collapse ">
+                        <input type="checkbox"> 
+                        <div class="collapse-title text-xl font-medium">
+                            I open with focus
+                        </div>
+                        <div class="collapse-content">
+                            <p>Collapse content reveals with focus. If you add a checkbox, you can control it using checkbox
+                                instead of focus. Or you can force-open/force-close using
+                                <span class="badge badge-outline">collapse-open</span> and
+                                <span class="badge badge-outline">collapse-close</span> classes.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="grid grid-cols-4">
+            <div>dsfsd</div>
+            <div>dsfsd</div>
+
+
+        </div>
+    </section>
+</main>
